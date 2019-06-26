fbfb2418371a70194bb94767193ac2d0a1c285ac
LiuQi <2584641446@qq.com>
2019-06-26 20:54:30 +0800

    Update fashion_mnist.py

--- example/fashion_mnist.py
+++ example/fashion_mnist.py
@@ -45,7 +45,32 @@ print(classifications[0])
  2.3528349e-03 2.7860409e-07 1.9346710e-02 9.2136581e-08 9.7829986e-01]
  ```
  
+#############################  Callback
+import tensorflow as tf
 
+class myCallback(tf.keras.callbacks.Callback):     # 定义Callback类
+  def on_epoch_end(self, epoch, logs={}):          # 调用 on_epoch_end 函数：在每次迭代后执行
+    if(logs.get('acc')>0.6):
+      print("\nReached 60% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+mnist = tf.keras.datasets.fashion_mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+callbacks = myCallback()                           #类实例化
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])   #callbacks=[callbacks] ，第一个callbacks是 model.fit的参数
 
 
 
