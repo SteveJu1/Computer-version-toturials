584d3f45dd7eaf9e18ce9984129f37357a969274
LiuQi <2584641446@qq.com>
2019-06-27 11:45:37 +0800

    Update Convolution.py

--- example/Convolution.py
+++ example/Convolution.py
@@ -8,8 +8,6 @@ mnist=tf.keras.datasets.mnist
 num=np.array(training_images)
 print(num.shape)
 
-
-
 (train_set,train_label),(test_set,test_label)=mnist.load_data()
 train_set=train_set.reshape(60000,28,28,1)         #将数据放在一个Tensor里，才能 convolution
 train_set=train_set/255.0
@@ -45,6 +43,26 @@ _________________________________________________________________
 dense_1 (Dense)              (None, 10)                1290      
 =================================================================
 ```
-
+####### 显示卷积层，池化层的输出
 model.fit(train_set,train_label,epochs=5)
 test_loss,test_acc=model.evaluate(test_set)     # valuate	英[ɪˈvæljueɪt]    model.evaluate会返回两个值，若只有一个变量接受会返回矩阵
+                 
+import matplotlib.pyplot as plt
+f, axarr = plt.subplots(3,4)
+FIRST_IMAGE=0
+SECOND_IMAGE=23
+THIRD_IMAGE=55
+CONVOLUTION_NUMBER =4
+from tensorflow.keras import models
+layer_outputs = [layer.output for layer in model.layers]
+activation_model = tf.keras.models.Model(inputs = model.input, outputs = layer_outputs)
+for x in range(0,4):
+  f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[0,x].imshow(f1[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[0,x].grid(False)
+  f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[1,x].imshow(f2[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[1,x].grid(False)
+  f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[2,x].imshow(f3[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[2,x].grid(False)
