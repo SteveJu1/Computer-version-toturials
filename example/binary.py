b8fcf99599808a1d5446df0d6bc77e3ef17aa1ca
LiuQi <2584641446@qq.com>
2019-06-27 21:08:04 +0800

    Update binary.py

--- example/binary.py
+++ example/binary.py
@@ -16,7 +16,6 @@ class myCallback(tf.keras.callbacks.Callback):
     if(logs.get('acc')>0.999):
       print('end')
       self.model.stop_training=True
-
 callbacks = myCallback()
 
 # This Code Block should Define and Compile the Model
@@ -33,9 +32,18 @@ model = tf.keras.models.Sequential([
                                     tf.keras.layers.Dense(256,activation='relu'),
                                     tf.keras.layers.Dense(1,activation='sigmoid')
 ])
-
+model.summary()
 from tensorflow.keras.optimizers import RMSprop
 model.compile(optimizer=RMSprop(lr=0.001),loss='binary_crossentropy',metrics=['acc'])
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1/255)
+train_generator = train_datagen.flow_from_directory(
+        "/tmp/h-or-s",  
+        target_size=(150, 150),        # 将加载的图像变成150*150
+        batch_size=10,
+        class_mode='binary')           #二分类
+
 # This code block should call model.fit_generator and train for a number of epochs. 
 history = model.fit_generator(
                             train_generator,
