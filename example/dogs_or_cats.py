7b59a76d4c9fdd5bf780017767ae4e6d967a0898
LiuQi <2584641446@qq.com>
2019-06-28 22:29:16 +0800

    Create dogs_or_cats.py

---
+++ example/dogs_or_cats.py
@@ -0,0 +1,157 @@
+```
+Data from kaggle 
+
+```
+import tensorflow as tf
+import os
+import zipfile
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from shutil import copyfile         # shell utility 
+
+```colab 获取网络数据的方式，如在本地可使用 urllib.request (Uniform Resource Identifier，统一资源标识符)或本地路径
+!wget --no-check-certificate \
+    "https://download.microsoft.com/download/3/E/1/3E1C3F21-ECDB-4869-8368-6DEBA77B919F/kagglecatsanddogs_3367a.zip" \
+    -O "/tmp/cats-and-dogs.zip"
+```
+local_zip = '/tmp/cats-and-dogs.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp')
+zip_ref.close()
+
+print(len(os.listdir('/tmp/PetImages/Cat/')))   # os.listdir('/tmp/PetImages/Cat/')是list
+# 建文件目录
+try:
+    os.mkdir('/tmp/cats-v-dogs')
+    os.mkdir('/tmp/cats-v-dogs/training')
+    os.mkdir('/tmp/cats-v-dogs/testing')
+    os.mkdir('/tmp/cats-v-dogs/training/cats')
+    os.mkdir('/tmp/cats-v-dogs/training/dogs')
+    os.mkdir('/tmp/cats-v-dogs/testing/cats')
+    os.mkdir('/tmp/cats-v-dogs/testing/dogs')
+except OSError:
+    pass   
+#  
+# os.listdir(DIRECTORY) gives you a listing of the contents of that directory
+# os.path.getsize(PATH) gives you the size of the file
+# copyfile(source, destination) copies a file from source to destination
+# random.sample(list, len(list)) shuffles a list
+def split_data(SOURCE, TRAINING, TESTING, SPLIT_SIZE):
+    files = []
+    for filename in os.listdir(SOURCE):
+        file = SOURCE + filename
+        if os.path.getsize(file) > 0:
+            files.append(filename)
+        else:
+            print(filename + " is zero length, so ignoring.")
+
+    training_length = int(len(files) * SPLIT_SIZE)
+    testing_length = int(len(files) - training_length)
+    shuffled_set = random.sample(files, len(files))
+    training_set = shuffled_set[0:training_length]
+    testing_set = shuffled_set[-testing_length:]
+
+    for filename in training_set:
+        this_file = SOURCE + filename
+        destination = TRAINING + filename
+        copyfile(this_file, destination)
+
+    for filename in testing_set:
+        this_file = SOURCE + filename
+        destination = TESTING + filename
+        copyfile(this_file, destination)
+
+
+CAT_SOURCE_DIR = "/tmp/PetImages/Cat/"
+TRAINING_CATS_DIR = "/tmp/cats-v-dogs/training/cats/"
+TESTING_CATS_DIR = "/tmp/cats-v-dogs/testing/cats/"
+DOG_SOURCE_DIR = "/tmp/PetImages/Dog/"
+TRAINING_DOGS_DIR = "/tmp/cats-v-dogs/training/dogs/"
+TESTING_DOGS_DIR = "/tmp/cats-v-dogs/testing/dogs/"
+
+split_size = .9
+split_data(CAT_SOURCE_DIR, TRAINING_CATS_DIR, TESTING_CATS_DIR, split_size)
+split_data(DOG_SOURCE_DIR, TRAINING_DOGS_DIR, TESTING_DOGS_DIR, split_size)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer=RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['acc'])
+
+TRAINING_DIR = "/tmp/cats-v-dogs/training/"
+train_datagen = ImageDataGenerator(rescale=1.0/255.)
+train_generator = train_datagen.flow_from_directory(TRAINING_DIR,
+                                                    batch_size=100,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))
+
+VALIDATION_DIR = "/tmp/cats-v-dogs/testing/"
+validation_datagen = ImageDataGenerator(rescale=1.0/255.)
+validation_generator = validation_datagen.flow_from_directory(VALIDATION_DIR,
+                                                              batch_size=100,
+                                                              class_mode='binary',
+                                                              target_size=(150, 150))
+history = model.fit_generator(train_generator,
+                              epochs=50,
+                              verbose=1,
+                              validation_data=validation_generator)
+import matplotlib.image  as mpimg
+import matplotlib.pyplot as plt
+
+#-----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+#-----------------------------------------------------------
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs=range(len(acc)) # Get number of epochs
+
+#------------------------------------------------
+# Plot training and validation accuracy per epoch
+#------------------------------------------------
+plt.plot(epochs, acc, 'r', "Training Accuracy")
+plt.plot(epochs, val_acc, 'b', "Validation Accuracy")
+plt.title('Training and validation accuracy')
+plt.figure()
+
+#------------------------------------------------
+# Plot training and validation loss per epoch
+#------------------------------------------------
+plt.plot(epochs, loss, 'r', "Training Loss")
+plt.plot(epochs, val_loss, 'b', "Validation Loss")
+plt.figure()
+# Here's a codeblock just for fun. You should be able to upload an image here 
+# and have it classified without crashing
+import numpy as np
+from google.colab import files
+from keras.preprocessing import image
+
+uploaded = files.upload()
+
+for fn in uploaded.keys():
+ 
+  # predicting images
+  path = '/content/' + fn
+  img = image.load_img(path, target_size=(150, 150))
+  x = image.img_to_array(img)
+  x = np.expand_dims(x, axis=0)
+
+  images = np.vstack([x])
+  classes = model.predict(images, batch_size=10)
+  print(classes[0])
+  if classes[0]>0.5:
+    print(fn + " is a dog")
+  else:
+    print(fn + " is a cat")                            
